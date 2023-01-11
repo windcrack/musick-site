596b63cd2df33c7426f0a910f20c3f96674570bf
windcrack <windcrack@mail.ru>
2023-01-11 21:22:45 +0300

    finish galery page

--- galery.php
+++ galery.php
@@ -3,7 +3,7 @@
 <main class="container">
     <section class="galery">
         <h1 class="section-title">Галерея</h1>
-        <div class="galery-new">
+        <section class="galery-new">
             <h2 class="section-title">Новое на сайте</h2>
             <ul class="galery-new__list">
                 <li class="galery-new__item">
@@ -37,43 +37,73 @@
                     </a>
                 </li>
             </ul>
-        </div>
-        <div class="galery-news">
+        </section>
+        <section class="galery-news">
             <div class="galery-news__body">
                 <div class="galery-news__row">
-                    <div class="galery-news__block galery-news__block_large">
+                    <a href="#" class="galery-news__block galery-news__block_large">
                         <img src="img/galery/img1.jpg" alt="" class="galery-news__img">
                         <div class="galery-news__text">
-                            <div class="galery-news__text-title"></div>
-                            <time class="galery-news__text-date"></time>
+                            <div class="galery-news__text-title">Lorem ipsum dolor sit.</div>
+                            <time class="galery-news__text-date"><?= date("d.m.Y"); ?></time>
                         </div>
-                    </div>
-                    <div class="galery-news__block galery-news__block_smal">
+                    </a>
+                    <a href="#" class="galery-news__block galery-news__block_smal">
                         <img src="img/galery/img2.jpg" alt="" class="galery-news__img">
                         <div class="galery-news__text">
-                            <div class="galery-news__text-title"></div>
-                            <time class="galery-news__text-date"></time>
+                            <div class="galery-news__text-title">Lorem ipsum dolor sit.</div>
+                            <time class="galery-news__text-date"><?= date("d.m.Y"); ?></time>
                         </div>
-                    </div>
+                    </a>
                 </div>
                 <div class="galery-news__row">
-                    <div class="galery-news__block galery-news__block">
-                        <img src="img/galery/img3.jpg" alt="" class="galery-news__img">
-                        <div class="galery-news__text">
-                            <div class="galery-news__text-title"></div>
-                            <time class="galery-news__text-date"></time>
-                        </div>
-                    </div>
-                    <div class="galery-news__block galery-news__block">
+                    <a href="#" class="galery-news__block galery-news__block">
                         <img src="img/galery/img4.jpg" alt="" class="galery-news__img">
                         <div class="galery-news__text">
-                            <div class="galery-news__text-title"></div>
-                            <time class="galery-news__text-date"></time>
+                            <div class="galery-news__text-title">Lorem ipsum dolor sit.</div>
+                            <time class="galery-news__text-date"><?= date("d.m.Y"); ?></time>
                         </div>
-                    </div>
+                    </a>
+                    <a href="#" class="galery-news__block galery-news__block">
+                        <img src="img/galery/img3.jpg" alt="" class="galery-news__img">
+                        <div class="galery-news__text">
+                            <div class="galery-news__text-title">Lorem ipsum dolor sit.</div>
+                            <time class="galery-news__text-date"><?= date("d.m.Y"); ?></time>
+                        </div>
+                    </a>
+                </div>
+                <button class="btn btn__galery">Больше фото</button>
+            </div>
+        </section>
+        <section class="galery-new-video">
+            <h2 class="section-title">Новые видео</h2>
+            <div class="galery-new-video__body">
+                <div class="galery-new-video__block">
+                    <iframe width="100%" height="360" src="https://www.youtube.com/embed/FvDKUkLQ5RA" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+                <div class="galery-new-video__block">
+                    <iframe width="100%" height="360" src="https://www.youtube.com/embed/FvDKUkLQ5RA" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+                <div class="galery-new-video__block">
+                    <iframe width="100%" height="360" src="https://www.youtube.com/embed/FvDKUkLQ5RA" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+                <div class="galery-new-video__block">
+                    <iframe width="100%" height="360" src="https://www.youtube.com/embed/FvDKUkLQ5RA" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+                <div class="galery-new-video__block">
+                    <iframe width="100%" height="360" src="https://www.youtube.com/embed/FvDKUkLQ5RA" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+                <div class="galery-new-video__block">
+                    <iframe width="100%" height="360" src="https://www.youtube.com/embed/FvDKUkLQ5RA" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+                <div class="galery-new-video__block">
+                    <iframe width="100%" height="360" src="https://www.youtube.com/embed/FvDKUkLQ5RA" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+                <div class="galery-new-video__block">
+                    <iframe width="100%" height="360" src="https://www.youtube.com/embed/FvDKUkLQ5RA" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 </div>
             </div>
-        </div>
+        </section>
     </section>
 
 <?php include_once 'parts/footer.php' ?>
